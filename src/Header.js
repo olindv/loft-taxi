41fc94329852481f1da77e1a2f18c09198414275
dmitry olin <dolin@synergy.ru>
2020-09-21 23:27:31 +0300

    fixed header test

--- src/Header.js
+++ src/Header.js
@@ -5,59 +5,50 @@ import { string, func } from "prop-types";
 import { AuthContext } from "./App";
 
 const propTypes = {
-    pageName: string,
-    changePage: func,
+  pageName: string,
+  changePage: func,
 };
 
 const Header = ({ changePage, pageName }) => {
-    const context = useContext(AuthContext);
-    const logoutButton = () => {
-        context.logout();
-    };
+  const context = useContext(AuthContext);
+  const logoutButton = () => {
+    context.logout();
+  };
 
-    const headerButtons = [
-        { name: "map", text: "Карта" },
-        { name: "profile", text: "Профиль" },
-        { name: "login", text: context.isLoggedIn ? "Войти" : "Выйти" },
-    ];
+  const headerButtons = [
+    { name: "map", text: "Карта" },
+    { name: "profile", text: "Профиль" },
+    { name: "login", text: context.isLoggedIn ? "Войти" : "Выйти" },
+  ];
 
-    console.log(context.isLoggedIn);
-
-    return (
-        <>
-            {(pageName === "map" || pageName === "profile") && (
-                <header className="header" data-testid="header">
-                    <div className="header__container container">
-                        <div className="header__logo">
-                            <Logo />
-                        </div>
-                        <nav className="header__navigation">
-                            <ul className="header__navigation-list">
-                                {headerButtons.map(({ name, text }) => (
-                                    <li
-                                        className="header__navigation-item"
-                                        key={name}
-                                    >
-                                        <button
-                                            name={name}
-                                            onClick={
-                                                name === "login"
-                                                    ? logoutButton
-                                                    : changePage
-                                            }
-                                            className="header__navigation-button"
-                                        >
-                                            {text}
-                                        </button>
-                                    </li>
-                                ))}
-                            </ul>
-                        </nav>
-                    </div>
-                </header>
-            )}
-        </>
-    );
+  return (
+    <>
+      {(pageName === "map" || pageName === "profile") && (
+        <header className="header" data-testid="header">
+          <div className="header__container container">
+            <div className="header__logo">
+              <Logo />
+            </div>
+            <nav className="header__navigation">
+              <ul className="header__navigation-list">
+                {headerButtons.map(({ name, text }) => (
+                  <li className="header__navigation-item" key={name}>
+                    <button
+                      name={name}
+                      onClick={name === "login" ? logoutButton : changePage}
+                      className="header__navigation-button"
+                    >
+                      {text}
+                    </button>
+                  </li>
+                ))}
+              </ul>
+            </nav>
+          </div>
+        </header>
+      )}
+    </>
+  );
 };
 
 Header.propTypes = propTypes;
--- src/Header.test.js
+++ src/Header.test.js
@@ -1,10 +1,17 @@
 import React from "react";
 import Header from "./Header";
 import { render } from "@testing-library/react";
+import { AuthContext } from "./App";
 
 describe("Header", () => {
-    it("renders correctly", () => {
-        const { getByTestId } = render(<Header />);
-        // expect(getByTestId("header")).toBeInTheDocument();
-    });
+  it("renders correctly", () => {
+    const { getByTestId } = render(
+      <AuthContext.Provider
+        value={{ login: () => {}, logout: () => {}, isLoggedIn: false }}
+      >
+        <Header pageName="map" />
+      </AuthContext.Provider>
+    );
+    expect(getByTestId("header")).toBeInTheDocument();
+  });
 });
