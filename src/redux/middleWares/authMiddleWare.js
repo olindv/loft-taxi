549bda74cf02ac060250e0d49d91dd5dec4dd35c
dmitry olin <dolin@synergy.ru>
2020-09-28 23:11:49 +0300

    fix reducer

--- src/redux/middleWares/authMiddleWare.js
+++ src/redux/middleWares/authMiddleWare.js
@@ -1,44 +1,44 @@
 import {
-    loginRequest,
-    loginSuccess,
-    loginFailure,
-    registrationRequest,
-    registrationSuccess,
-    registrationFailure,
-    logoutUser,
+  loginRequest,
+  loginSuccess,
+  loginFailure,
+  registrationRequest,
+  registrationSuccess,
+  registrationFailure,
+  logoutUser,
 } from "../actions/actions";
 import { serverLogin, serverRegistration } from "../../api.js";
 
 export const authMiddleWare = (store) => (next) => async (action) => {
-    if (action.type === loginRequest().type) {
-        console.log("action:", action);
-        const { email, password } = action.payload;
-        const [success, token] = await serverLogin(email, password);
-        if (success) {
-            store.dispatch(loginSuccess());
-            window.localStorage.setItem("token", token);
-        } else {
-            store.dispatch(loginFailure());
-        }
-    } else if (action.type === registrationRequest().type) {
-        const { name, surname, email, password } = action.payload;
-        const [success, token] = await serverRegistration(
-            name,
-            surname,
-            email,
-            password
-        );
-        if (success) {
-            store.dispatch(registrationSuccess());
-            window.localStorage.setItem("token", token);
-        } else {
-            store.dispatch(registrationFailure());
-        }
-    } else if (action.type === logoutUser().type) {
-        console.log("action:", action, store.getState());
-        store.dispatch(logoutUser());
-        window.localStorage.removeItem("token");
+  if (action.type === loginRequest().type) {
+    console.log("action:", action);
+    const { email, password } = action.payload;
+    const [success, token] = await serverLogin(email, password);
+    if (success) {
+      store.dispatch(loginSuccess());
+      window.localStorage.setItem("token", token);
     } else {
-        next(action);
+      store.dispatch(loginFailure());
     }
+  } else if (action.type === registrationRequest().type) {
+    const { name, surname, email, password } = action.payload;
+    const [success, token] = await serverRegistration(
+      name,
+      surname,
+      email,
+      password
+    );
+    if (success) {
+      store.dispatch(registrationSuccess());
+      window.localStorage.setItem("token", token);
+    } else {
+      store.dispatch(registrationFailure());
+    }
+  } else if (action.type === logoutUser().type) {
+    console.log("action:", action, store.getState());
+    window.localStorage.removeItem("token");
+    next(action);
+  } else {
+    next(action);
+  }
 };
--- src/redux/reducers/authReducer.js
+++ src/redux/reducers/authReducer.js
@@ -1,22 +1,21 @@
 export default (state = false, action) => {
-    switch (action.type) {
-        case "LOGIN_REQUEST":
-            return action.payload;
-        case "LOGIN_SUCCESS":
-            return true;
-        case "LOGIN_FAILURE":
-            return false;
-        case "REGISTRATION_REQUEST":
-            return action.payload;
-        case "REGISTRATION_SUCCESS":
-            return true;
-        case "REGISTRATION_FAILURE":
-            return false;
-        case "LOGOUT_USER":
-            debugger;
-            return false;
+  switch (action.type) {
+    case "LOGIN_REQUEST":
+      return action.payload;
+    case "LOGIN_SUCCESS":
+      return true;
+    case "LOGIN_FAILURE":
+      return false;
+    case "REGISTRATION_REQUEST":
+      return action.payload;
+    case "REGISTRATION_SUCCESS":
+      return true;
+    case "REGISTRATION_FAILURE":
+      return false;
+    case "LOGOUT_USER":
+      return false;
 
-        default:
-            return state;
-    }
+    default:
+      return state;
+  }
 };
