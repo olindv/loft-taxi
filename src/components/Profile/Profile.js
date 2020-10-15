1d4b89cc0a950773e8210861c466282fee9c26ab
dmitry olin <dolin@synergy.ru>
2020-10-15 23:45:27 +0300

    change profile form

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,56 +2,28 @@ import React from "react";
 
 import "./Profile.scss";
 import { connect } from "react-redux";
-import {
-    paymentRequest,
-    paymentChangeField,
-} from "../../redux/actions/actions";
-
 import { ProfileForm } from "./ProfileForm";
 
-const Profile = ({ payment, inputValues, changeField }) => {
-    // const handleInnputChange = (e) => {
-    //   const { name, value } = e.target;
-    //   changeField({ name, value });
-    // };
-    // const handleDateChange = (date) => {
-    //   changeField({ name: "expiryDate", value: date });
-    // };
-
-    // const submit = (e) => {
-    //   e.preventDefault();
-    //   payment(inputValues);
-    // };
-
-    return (
-        <div className="profile" data-testid="profile">
-            <div className="profile__container">
-                <div className="profile__wrap">
-                    <div className="profile__head">
-                        <h2 className="profile__title">Профиль</h2>
-                        <div className="profile__subtitle">Способ оплаты</div>
-                    </div>
-                    <ProfileForm />
-                </div>
-            </div>
+const Profile = ({ inputValues }) => {
+  return (
+    <div className="profile" data-testid="profile">
+      <div className="profile__container">
+        <div className="profile__wrap">
+          <div className="profile__head">
+            <h2 className="profile__title">Профиль</h2>
+            <div className="profile__subtitle">Способ оплаты</div>
+          </div>
+          <ProfileForm inputValues={inputValues} />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        inputValues: state.payment,
-    };
+  return {
+    inputValues: state.payment,
+  };
 };
-// const mapDispatchToProps = (dispatch) => {
-//   return {
-//     payment: (cardNumber, expiryDate, userName, cvcNumber, token) =>
-//       dispatch(
-//         paymentRequest({ cardNumber, expiryDate, userName, cvcNumber, token })
-//       ),
-//     changeField: (payload) => {
-//       dispatch(paymentChangeField(payload));
-//     },
-//   };
-// };
+
 export default connect(mapStateToProps, null)(Profile);
--- src/components/Profile/ProfileForm.jsx
+++ src/components/Profile/ProfileForm.jsx
@@ -1,128 +1,126 @@
 import React from "react";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import {
-    paymentRequest,
-    paymentChangeField,
+  paymentRequest,
+  paymentChangeField,
 } from "../../redux/actions/actions";
-import { Link } from "react-router-dom";
 import { MCIcon } from "loft-taxi-mui-theme";
 import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
+import { TextField } from "@material-ui/core";
 import DateFnsUtils from "@date-io/date-fns";
-import { connect } from "react-redux";
 
-export const ProfileForm = ({ payment, changeField, inputValues }) => {
-    const handleInnputChange = (e) => {
-        const { name, value } = e.target;
-        changeField({ name, value });
-    };
-    const handleDateChange = (date) => {
-        changeField({ name: "expiryDate", value: date });
-    };
+export const ProfileForm = ({ inputValues }) => {
+  const { control, handleSubmit } = useForm();
+  const dispatch = useDispatch();
 
-    const { register, handleSubmit } = useForm();
-    const dispatch = useDispatch();
-    const onSubmit = (data) => {
-        const { name, surname, email, password } = data;
-        dispatch(paymentRequest({ name, surname, email, password }));
-    };
+  const handleInnputChange = (e) => {
+    const { name, value } = e.target;
+    dispatch(paymentChangeField({ name, value }));
+  };
+  const handleDateChange = (date) => {
+    dispatch(paymentChangeField({ name: "expiryDate", value: date }));
+  };
 
-    return (
-        <form className="form" onSubmit={handleSubmit(onSubmit)}>
-            <div className="form__container">
-                <div className="form__left">
-                    <div className="icon">
-                        <MCIcon />
-                    </div>
-                    <div className="form__row form__row_column">
-                        <label htmlFor="cardNumber">Номер карты:</label>
-                        <input
-                            ref={register}
-                            type="cardNumber"
-                            value={inputValues.cardNumber}
-                            id="cardNumber"
-                            name="cardNumber"
-                            className="form__input"
-                            placeholder="0000 0000 0000 0000"
-                            onChange={handleInnputChange}
-                            required={true}
-                        ></input>
-                    </div>
+  const onSubmit = (e) => {
+    dispatch(paymentRequest());
+  };
+  return (
+    <form className="form" onSubmit={handleSubmit(onSubmit)}>
+      <div className="form__container">
+        <div className="form__left">
+          <div className="icon">
+            <MCIcon />
+          </div>
+          <div className="form__row form__row_column">
+            <Controller
+              id="cardNumber"
+              name="cardNumber"
+              rules={{ required: true }}
+              control={control}
+              defaultValue={inputValues.cardNumber}
+              render={() => {
+                return (
+                  <TextField
+                    id="cardNumber"
+                    label="Номер карты"
+                    type="cardNumber"
+                    fullWidth
+                    placeholder="0000 0000 0000 0000"
+                    name="cardNumber"
+                    value={inputValues.cardNumber}
+                    onChange={handleInnputChange}
+                  />
+                );
+              }}
+            />
+          </div>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <Controller
+              name="expiryDate"
+              rules={{ required: true }}
+              control={control}
+              defaultValue={inputValues.expiryDate}
+              render={() => (
+                <DatePicker
+                  value={inputValues.expiryDate}
+                  onChange={handleDateChange}
+                  label="Срок действия:"
+                  minDate={new Date()}
+                  placeholder="07/22"
+                  openTo="year"
+                  views={["year", "month"]}
+                  format="MM/yy"
+                  fullWidth
+                />
+              )}
+            />
+          </MuiPickersUtilsProvider>
+        </div>
+        <div className="form__right">
+          <div className="form__row form__row_column">
+            <Controller
+              name="cardName"
+              control={control}
+              rules={{ required: true }}
+              defaultValue={inputValues.cardName}
+              render={() => (
+                <TextField
+                  label="Имя владельца"
+                  type="cardName"
+                  fullWidth
+                  name="cardName"
+                  placeholder="USER NAME"
+                  value={inputValues.cardName}
+                  onChange={handleInnputChange}
+                />
+              )}
+            />
+          </div>
+          <div className="form__row form__row_column">
+            <Controller
+              name="cvc"
+              control={control}
+              rules={{ required: true }}
+              defaultValue={inputValues.cvc}
+              render={() => (
+                <TextField
+                  label="CVC*"
+                  type="cvc"
+                  fullWidth
+                  name="cvc"
+                  value={inputValues.cvc}
+                  onChange={handleInnputChange}
+                />
+              )}
+            />
+          </div>
+        </div>
+      </div>
 
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                        <DatePicker
-                            ref={register}
-                            name="expiryDate"
-                            value={inputValues.expiryDate}
-                            onChange={handleDateChange}
-                            label="Срок действия:"
-                            minDate={new Date()}
-                            placeholder="07/22"
-                            openTo="year"
-                            views={["year", "month"]}
-                            format="MM/yy"
-                            fullWidth
-                        />
-                    </MuiPickersUtilsProvider>
-                </div>
-                <div className="form__right">
-                    <div className="form__row form__row_column">
-                        <label htmlFor="cardName">Имя владельца:</label>
-                        <input
-                            ref={register}
-                            type="cardName"
-                            id="cardName"
-                            name="cardName"
-                            value={inputValues.cardName}
-                            className="form__input"
-                            onChange={handleInnputChange}
-                            required={true}
-                        ></input>
-                    </div>
-                    <div className="form__row form__row_column">
-                        <label htmlFor="cvc">CVC:</label>
-                        <input
-                            ref={register}
-                            type="cvc"
-                            id="cvc"
-                            name="cvc"
-                            // value={inputValues.cvc}
-                            className="form__input"
-                            onChange={handleInnputChange}
-                            required={true}
-                        ></input>
-                    </div>
-                </div>
-            </div>
-
-            <button type="submit" className="form__button">
-                Сохранить
-            </button>
-        </form>
-    );
+      <button type="submit" className="form__button">
+        Сохранить
+      </button>
+    </form>
+  );
 };
-
-const mapStateToProps = (state) => {
-    return {
-        inputValues: state.payment,
-    };
-};
-const mapDispatchToProps = (dispatch) => {
-    return {
-        payment: (cardNumber, expiryDate, userName, cvcNumber, token) =>
-            dispatch(
-                paymentRequest({
-                    cardNumber,
-                    expiryDate,
-                    userName,
-                    cvcNumber,
-                    token,
-                })
-            ),
-        changeField: (payload) => {
-            dispatch(paymentChangeField(payload));
-        },
-    };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(ProfileForm);
