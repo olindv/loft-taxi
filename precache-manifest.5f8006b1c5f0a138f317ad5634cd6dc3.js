c87a4d1542bcbcdd4f5ea2de21b5d4e56b004ef6
dmitry olin <1988navras@gmail.com>
2021-06-13 22:01:50 +0300

    Updates

---
+++ precache-manifest.5f8006b1c5f0a138f317ad5634cd6dc3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bb6cfa00a4c8f5ee3ae1adbbba89361",
+    "url": "/loft-taxi/index.html"
+  },
+  {
+    "revision": "b31583632baa4c71f6ae",
+    "url": "/loft-taxi/static/css/main.a94092ff.chunk.css"
+  },
+  {
+    "revision": "71d4a1a4f2a965291a4d",
+    "url": "/loft-taxi/static/js/2.bca6d746.chunk.js"
+  },
+  {
+    "revision": "eb4771dcc96ac79e289ef5805b151a7b",
+    "url": "/loft-taxi/static/js/2.bca6d746.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b31583632baa4c71f6ae",
+    "url": "/loft-taxi/static/js/main.820ac285.chunk.js"
+  },
+  {
+    "revision": "5db8eea055306e2e5efd",
+    "url": "/loft-taxi/static/js/runtime-main.9fadced3.js"
+  },
+  {
+    "revision": "eb556b48df19e2b33d4b00b6edabd0a1",
+    "url": "/loft-taxi/static/media/bg.eb556b48.jpg"
+  }
+]);
